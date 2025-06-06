4ef03b23c34371fa33627197edc81a7894c8e3a5
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-06-06 13:21:02 -0400

    Implemented JavaScript to bring navbar and footer to each HTML file.

---
+++ JS/inject.js
@@ -0,0 +1,17 @@
+async function loadHTML(selector, file) {
+  const element = document.querySelector(selector);
+  if (element) {
+    try {
+      const res = await fetch(file);
+      const html = await res.text();
+      element.innerHTML = html;
+    } catch (err) {
+      console.error(`Could not load ${file}:`, err);
+    }
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadHTML("#navbar", "/PARTIALS/navbar.html");
+  loadHTML("#footer", "/PARTIALS/footer.html");
+});
